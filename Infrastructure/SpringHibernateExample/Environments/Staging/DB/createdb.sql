6769061f23bea6f792f59ebdc7e6f8f20dea88b6
David Carbajosa <dcarbajosa@gmail.com>
2016-02-12 18:46:58 +0000

    update createdb

--- Infrastructure/SpringHibernateExample/Environments/Staging/DB/createdb.sql
+++ Infrastructure/SpringHibernateExample/Environments/Staging/DB/createdb.sql
@@ -1,4 +1,4 @@
-CREATE TABLE EMPLOYEE(
+CREATE TABLE Employee(
     id INT NOT NULL auto_increment, 
     name VARCHAR(50) NOT NULL,
     joining_date DATE NOT NULL,
@@ -7,5 +7,5 @@ CREATE TABLE EMPLOYEE(
     PRIMARY KEY (id)
 );
 
-INSERT INTO EMPLOYEE ( id, name, joining_date, salary, ssn ) VALUES ( uuid(), 'Jane Smith', '2002-10-08', 5000.00, '11111111111' );
+INSERT INTO Employee ( id, name, joining_date, salary, ssn ) VALUES ( uuid(), 'Jane Smith', '2002-10-08', 5000.00, '11111111111' );
 /* INSERT INTO EMPLOYEE ( id, name, joining_date, salary, ssn ) VALUES ( uuid(), 'Dave Richards','2003-10-08', 15000.00, '11111111112' );*/
